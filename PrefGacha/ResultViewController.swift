735a366071ee0d0898d9fdd60ed55823e9116cf2
s-2010-119 <wisteria1349@gmail.com>
2020-08-22 20:32:17 +0900

    create Main.storyboard

---
+++ PrefGacha/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  PrefGacha
+//
+//  Created by Nancy on 2020/08/22.
+//  Copyright © 2020 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PrefGacha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrefGacha
+//
+//  Created by Nancy on 2020/08/22.
+//  Copyright © 2020 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
